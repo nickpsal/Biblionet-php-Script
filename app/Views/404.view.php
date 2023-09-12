d995c039e4b31082f75e46ca362ce8d707208fc4
NIKOLAOS PSALTAKHS <nickpsal@hotmail.com>
2023-09-12 16:41:18 +0300

    commit

--- app/Views/404.view.php
+++ app/Views/404.view.php
@@ -1,1 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class=" col-md-6 mx-auto">
+            <img src="<?=ROOT?>assets/img/juliet.jpg" class="img-fluid rounded" alt="Image" width="300">
+        </div>
+    </div>
+</div>
 <h1>404 Page error</h1>
--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -1,7 +1,8 @@
 <div class="container">
-    <div class="row>
-            <div class=" col-md-6 mx-auto">
-        <img src="assets/img/juliet.jpg" class="img-fluid rounded" alt="Image" width="300">
+    <div class="row">
+        <div class=" col-md-6 mx-auto">
+            <img src="<?=ROOT?>assets/img/juliet.jpg" class="img-fluid rounded" alt="Image" width="300">
+        </div>
     </div>
 </div>
 <div class="container">
