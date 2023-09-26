ef519bb29498a3e4d396dacb95ae24a3503fa790
NIKOLAOS PSALTAKHS <nickpsal@hotmail.com>
2023-09-26 17:00:08 +0300

    commit composer init

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require "../app/Core/init.php";
+    require "../vendor/autoload.php";
     DEBUG ? ini_set("display_errors", 1) : ini_set("display_errors", 0);
 
     $app = new App;
